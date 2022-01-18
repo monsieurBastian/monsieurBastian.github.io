7514eb3c823c4d93df81c52fbc34b9d69c7c699a
Bastian G <bastian.gurtner@gmail.com>
2022-01-18 17:33:01 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,74 @@
+/* INSTALL PACKAGES ON THE MACHINE */
+/*
+1. install the necessary packages in terminal
+    - node > node -v > v17.3.0
+    - express > npm list express > express@4.17.2
+    - body parser > npm list body-parser > body-parser@1.19.1
+    - cors > npm list cors > cors@2.8.5
+*/
+
+
+
+projectData = {};
+
+
+
+// Express to run the server
+// and start an instance of app
+const express = require('express');
+const app = express();
+
+
+
+/*** 
+ * 
+ * Middleware
+ * 
+ ***/
+
+// Configure express to use body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Allow cross origin with Cors
+const cors = require('cors');
+app.use(cors());
+
+// Init project folder
+app.use(express.static('website'));
+
+
+
+/***
+ * 
+ * Server
+ * 
+ */
+
+// run the server
+const port = 8000;
+const server = app.listen(port, () => {
+  console.log(`Running on localhost: ${port}`);
+});
+
+
+
+
+/***
+ * 
+ * Routes
+ * 
+ */
+
+// GET route to return the project data
+app.get("/", function(req, res) {
+  res.send(projectData);
+  console.log('the GET route');
+});
+
+// POST route
+app.post("/add", function(req, res) {
+  res.send('POST received');
+  console.log('the POST route');
+});
